8d41b90f2843c5ea6adfc6cc767c89f3df4d48cd
ppharris <ppharris@users.noreply.github.com>
2023-11-13 10:59:40 +0000

    Tidy inundation_mask_maps.py.

--- src/inundation_mask_maps.py
+++ src/inundation_mask_maps.py
@@ -2,7 +2,6 @@ import os
 from tqdm import tqdm
 import numpy as np
 import numpy.ma as ma
-from read_data_iris import read_data_all_years
 import iris.coord_categorisation
 from iris.time import PartialDateTime
 import cartopy.crs as ccrs
@@ -12,13 +11,16 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from mpl_toolkits.axes_grid1 import AxesGrid
-from plot_utils import binned_cmap
+
 from lag_by_land_cover import copernicus_land_cover
+from plot_utils import binned_cmap
+from read_data_iris import read_data_all_years
 
 
-def save_number_seasonal_vod_obs(vod_no_sw_mask, vod_sw_mask, season):
-    save_dir = '../data/number_vod_obs'
-    os.system(f'mkdir -p {save_dir}')
+def save_number_seasonal_vod_obs(output_dirs, vod_no_sw_mask, vod_sw_mask, season):
+
+    save_dir = output_dirs["number_obs"]
+
     vod_no_sw_mask_season = vod_no_sw_mask.extract(iris.Constraint(clim_season=season.lower()))
     vod_sw_mask_season = vod_sw_mask.extract(iris.Constraint(clim_season=season.lower()))
     total_obs_no_sw_mask = vod_no_sw_mask_season.collapsed('time', iris.analysis.COUNT,
@@ -31,7 +33,10 @@ def save_number_seasonal_vod_obs(vod_no_sw_mask, vod_sw_mask, season):
     np.save(f'{save_dir}/total_obs_sw_mask_{season}.npy', ma.filled(total_obs_sw_mask.data, 0.))
 
 
-def save_ssm_seasonal_obs_numbers():
+def save_ssm_seasonal_obs_numbers(output_dirs):
+
+    save_dir = output_dirs["number_obs"]
+
     ssm = read_data_all_years('SM', lon_west=-180, lon_east=180, 
                               lat_south=-55, lat_north=55, min_year=2000, max_year=2018)
     jun2000 = PartialDateTime(year=2000, month=6)
@@ -41,15 +46,13 @@ def save_ssm_seasonal_obs_numbers():
     iris.coord_categorisation.add_season(ssm, 'time', name='clim_season')
     seasons = ['MAM', 'JJA', 'SON', 'DJF']
     for season in tqdm(seasons, desc='saving SSM seasonal obs numbers'):
-        save_dir = '../data/number_vod_obs'
-        os.system(f'mkdir -p {save_dir}')
         ssm_season = ssm.extract(iris.Constraint(clim_season=season.lower()))
         total_obs_ssm = ssm_season.collapsed('time', iris.analysis.COUNT,
                                              function=lambda values: values > -99.)
         np.save(f'{save_dir}/total_obs_ssm_{season}.npy', ma.filled(total_obs_ssm.data, 0.))
 
 
-def save_all_seasonal_obs_numbers():
+def save_all_seasonal_obs_numbers(output_dirs):
     vod_no_sw_mask = read_data_all_years('VOD', band='X', lon_west=-180, lon_east=180, 
                                          lat_south=-55, lat_north=55, min_year=2000, max_year=2018,
                                          mask_surface_water=False)
@@ -65,23 +68,26 @@ def save_all_seasonal_obs_numbers():
     iris.coord_categorisation.add_season(vod_sw_mask, 'time', name='clim_season')
     seasons = ['MAM', 'JJA', 'SON', 'DJF']
     for season in tqdm(seasons, desc='saving seasonal obs numbers'):
-        save_number_seasonal_vod_obs(vod_no_sw_mask, vod_sw_mask, season)
-    save_ssm_seasonal_obs_numbers()
+        save_number_seasonal_vod_obs(output_dirs, vod_no_sw_mask, vod_sw_mask, season)
+    save_ssm_seasonal_obs_numbers(output_dirs)
 
 
-def removed_for_inundation(season):
-    save_dir = '../data/number_vod_obs'
-    total_possible_obs = np.load(f'{save_dir}/total_possible_obs_{season}.npy')
-    total_obs_no_sw_mask = np.load(f'{save_dir}/total_obs_no_sw_mask_{season}.npy')
-    total_obs_sw_mask = np.load(f'{save_dir}/total_obs_sw_mask_{season}.npy')
+def removed_for_inundation(number_obs_dir, season):
+    total_possible_obs = np.load(os.path.join(number_obs_dir, f'total_possible_obs_{season}.npy'))
+    total_obs_no_sw_mask = np.load(os.path.join(number_obs_dir, f'total_obs_no_sw_mask_{season}.npy'))
+    total_obs_sw_mask = np.load(os.path.join(number_obs_dir, f'total_obs_sw_mask_{season}.npy'))
     percent_before_mask = 100. * total_obs_no_sw_mask / total_possible_obs
     percent_after_mask = 100. * total_obs_sw_mask / total_possible_obs
     removed_by_mask = np.logical_and(percent_before_mask>=30., percent_after_mask<30.)
     return removed_by_mask
 
 
-def inundation_mask_maps():
-    cci_sm_mask = '../data/ESA-CCI-SOILMOISTURE-LAND_AND_RAINFOREST_MASK-fv04.2.nc' # this file is included in VODCA dataset
+def inundation_mask_maps(output_dirs):
+
+    number_obs_dir = output_dirs["number_obs"]
+    figures_dir = output_dirs["figures"]
+
+    cci_sm_mask = '/prj/nceo/bethar/VODCA_global/ESA-CCI-SOILMOISTURE-LAND_AND_RAINFOREST_MASK-fv04.2.nc' # this file is included in VODCA dataset
     lat55 = iris.Constraint(latitude=lambda cell: -55. <= cell.point <= 55.)
     land = np.flipud(iris.load_cube(cci_sm_mask, 'land').extract(lat55).data).astype(float)
     projection = ccrs.PlateCarree()
@@ -104,9 +110,9 @@ def inundation_mask_maps():
     seasons = ['MAM', 'JJA', 'SON', 'DJF']
 
     for i, ax in enumerate(axgr):
-        obs_removed = removed_for_inundation(seasons[i]).astype(float)
-        ssm_obs = np.load(f'../data/number_vod_obs/total_obs_ssm_{seasons[i]}.npy')
-        total_days = np.load(f'../data/number_vod_obs/total_possible_obs_{seasons[i]}.npy')
+        obs_removed = removed_for_inundation(number_obs_dir, seasons[i]).astype(float)
+        ssm_obs = np.load(os.path.join(number_obs_dir, f'total_obs_ssm_{seasons[i]}.npy'))
+        total_days = np.load(os.path.join(number_obs_dir, f'total_possible_obs_{seasons[i]}.npy'))
         ssm_obs_pc = 100. * ssm_obs / total_days
         ssm_obs_pc[land==0.] = np.nan
         obs_removed[obs_removed==0.] = np.nan
@@ -136,11 +142,21 @@ def inundation_mask_maps():
     cbar.set_ticks([1.25, 1.75])
     cbar.set_ticklabels(['masked', 'masked and no SSM obs'])
     cbar.ax.tick_params(labelsize=16) 
-    plt.savefig('../figures/pixels_removed_by_inundation_mask.pdf', dpi=1000, bbox_inches='tight')
-    plt.savefig('../figures/pixels_removed_by_inundation_mask.png', dpi=1000, bbox_inches='tight')
-    plt.show()
+    plt.savefig(os.path.join(figures_dir, 'pixels_removed_by_inundation_mask.pdf'), dpi=1000, bbox_inches='tight')
+    plt.savefig(os.path.join(figures_dir, 'pixels_removed_by_inundation_mask.png'), dpi=1000, bbox_inches='tight')
 
-    
+
 if __name__ == '__main__':
-    save_all_seasonal_obs_numbers()
-    inundation_mask_maps()
+
+    output_base_dir = "/path/to/output/dir"
+
+    output_dirs = {
+        "base": output_base_dir,
+        "spectra": os.path.join(output_base_dir, "csagan"),
+        "spectra_filtered": os.path.join(output_base_dir, "csagan_sig"),
+        "number_obs": os.path.join(output_base_dir, "number_obs_data"),
+        "figures": os.path.join(output_base_dir, "figures"),
+    }
+
+    save_all_seasonal_obs_numbers(output_dirs)
+    inundation_mask_maps(output_dirs)
