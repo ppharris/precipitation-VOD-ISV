9ce8205b548a89e913be498f10f487847f876671
ppharris <ppharris@users.noreply.github.com>
2024-01-05 16:21:37 +0000

    Get isv_composites_surface_water_filter_tiles.py running.

--- src/isv_composites_surface_water_filter_tiles.py
+++ src/isv_composites_surface_water_filter_tiles.py
@@ -7,6 +7,7 @@ import iris
 from read_data_iris import crop_cube
 import gc
 import string
+import os
 
 
 def land_cover_full_name(land_cover):
@@ -109,7 +110,8 @@ def composite_events(events, data_grid, days_range=60, existing_composite=None, 
     return days_around, composite, n
 
 
-def hemi_composites(hem, land_covers, days_range=60):
+def hemi_composites(output_dirs, hem, land_covers, days_range=60):
+
     if hem == 'north':
         lat_south = 0
         lat_north = 60
@@ -118,92 +120,178 @@ def hemi_composites(hem, land_covers, days_range=60):
         lat_north = 0
     else:
         raise KeyError(f'Hemisphere must be either north or south, not {hem}')
-    events = pickle.load(open(f'../data/imerg_isv_events_lowpass_1std_{hem}.p','rb'))
-    imerg_anomaly = iris.load(f'/scratch/bethar/daily_detrended_imerg_norm_anom_singleprecision_{hem}.nc')[0]
-    imerg_anom = ma.filled(imerg_anomaly.data, np.nan).astype(np.float32)
-    imerg_pad = np.ones((6940-imerg_anom.shape[0], imerg_anom.shape[1], imerg_anom.shape[2]), dtype=np.float32) * np.nan
+
+    data_isv_dir = output_dirs["data_isv"]
+
+    file_events = os.path.join(data_isv_dir, f'imerg_isv_events_lowpass_1std_{hem}.pkl')
+    with open(file_events, 'rb') as f:
+        events = pickle.load(f)
+
+    file_imerg = os.path.join(data_isv_dir, f'daily_detrended_imerg_norm_anom_singleprecision_{hem}.nc')
+    imerg_anomaly = iris.load_cube(file_imerg)
+
+    imerg_anom = imerg_anomaly.data.filled(np.nan)
+    imerg_pad_shape = (6940-imerg_anom.shape[0], imerg_anom.shape[1], imerg_anom.shape[2])
+    imerg_pad = np.full(imerg_pad_shape, np.nan, dtype=np.float32)
     imerg_anom = np.concatenate((imerg_pad, imerg_anom))
-    vod_anomaly = iris.load(f'/scratch/bethar/daily_detrended_vod_norm_anom_singleprecision_{hem}.nc')[0]
-    vod_anom = ma.filled(vod_anomaly.data.astype(np.float32), np.nan).astype(np.float32)
-    sm_anomaly = iris.load(f'/scratch/bethar/daily_detrended_sm_norm_anom_singleprecision_{hem}.nc')[0]
-    sm_anom = ma.filled(sm_anomaly.data.astype(np.float32), np.nan).astype(np.float32)
+
+    file_vod = os.path.join(data_isv_dir, f'daily_detrended_vod_norm_anom_singleprecision_{hem}.nc')
+    vod_anomaly = iris.load_cube(file_vod)
+    vod_anom = vod_anomaly.data.filled(np.nan)
+
+    file_sm = os.path.join(data_isv_dir, f'daily_detrended_sm_norm_anom_singleprecision_{hem}.nc')
+    sm_anomaly = iris.load_cube(file_sm)
+    sm_anom = sm_anomaly.data.filled(np.nan)
+
+    for arr in [imerg_anom, vod_anom, sm_anom]:
+        print(arr.shape)
+
+    data_comp_dir = output_dirs["data_isv_comp"]
+    
     for land_cover in tqdm(land_covers):
         lc_events = events_for_land_cover(events, land_cover, lat_south, lat_north)
         days_around, imerg_composite, imerg_n = composite_events_all_valid(lc_events, imerg_anom, imerg_anom, vod_anom, sm_anom, days_range=days_range)
         _, vod_composite, vod_n = composite_events_all_valid(lc_events, vod_anom, imerg_anom, vod_anom, sm_anom, days_range=days_range)
         _, sm_composite, sm_n = composite_events_all_valid(lc_events, sm_anom, imerg_anom, vod_anom, sm_anom, days_range=days_range)
+
         if np.logical_and(np.allclose(imerg_n, vod_n), np.allclose(imerg_n, sm_n)):
             n = imerg_n
         else:
             raise ValueError('ns are not the same')
-        np.save(f'../data/land_cover_isv_composites/imerg_composite_{hem}_55NS_{land_cover}.npy', imerg_composite)
-        np.save(f'../data/land_cover_isv_composites/vod_composite_{hem}_55NS_{land_cover}.npy', vod_composite)
-        np.save(f'../data/land_cover_isv_composites/sm_composite_{hem}_55NS_{land_cover}.npy', sm_composite)
-        np.save(f'../data/land_cover_isv_composites/n_{hem}_55NS_{land_cover}.npy', n)
+
+        file_out = os.path.join(data_comp_dir, f'imerg_composite_{hem}_55NS_{land_cover}.npy')
+        np.save(file_out, imerg_composite)
+
+        file_out = os.path.join(data_comp_dir, f'vod_composite_{hem}_55NS_{land_cover}.npy')
+        np.save(file_out, vod_composite)
+
+        file_out = os.path.join(data_comp_dir, f'sm_composite_{hem}_55NS_{land_cover}.npy')
+        np.save(file_out, sm_composite)
+
+        file_out = os.path.join(data_comp_dir, f'n_{hem}_55NS_{land_cover}.npy')
+        np.save(file_out, n)
 
 
-def full_composites(land_covers, days_range=60):
+def full_composites(output_dirs, land_covers, days_range=60):
     # must have already run hemi_composites('south')
-    events = pickle.load(open('../data/imerg_isv_events_lowpass_1std_north.p','rb'))
-    imerg_anomaly = iris.load('/scratch/bethar/daily_detrended_imerg_norm_anom_singleprecision_north.nc')[0]
-    imerg_anom = ma.filled(imerg_anomaly.data, np.nan).astype(np.float32)
-    imerg_pad = np.ones((6940-imerg_anom.shape[0], imerg_anom.shape[1], imerg_anom.shape[2]), dtype=np.float32) * np.nan
+
+    data_isv_dir = output_dirs["data_isv"]
+
+    file_events = os.path.join(data_isv_dir, 'imerg_isv_events_lowpass_1std_north.pkl')
+    with open(file_events, 'rb') as f:
+        events = pickle.load(f)
+
+    file_imerg = os.path.join(data_isv_dir, 'daily_detrended_imerg_norm_anom_singleprecision_north.nc')
+    imerg_anomaly = iris.load_cube(file_imerg)
+
+    imerg_anom = imerg_anomaly.data.filled(np.nan)
+    imerg_pad_shape = (6940-imerg_anom.shape[0], imerg_anom.shape[1], imerg_anom.shape[2])
+    imerg_pad = np.full(imerg_pad_shape, np.nan, dtype=np.float32)
     imerg_anom = np.concatenate((imerg_pad, imerg_anom))
-    vod_anomaly = iris.load('/scratch/bethar/daily_detrended_vod_norm_anom_singleprecision_north.nc')[0]
-    vod_anom = ma.filled(vod_anomaly.data.astype(np.float32), np.nan).astype(np.float32)
-    sm_anomaly = iris.load('/scratch/bethar/daily_detrended_sm_norm_anom_singleprecision_north.nc')[0]
-    sm_anom = ma.filled(sm_anomaly.data.astype(np.float32), np.nan).astype(np.float32)
+
+    file_vod = os.path.join(data_isv_dir, 'daily_detrended_vod_norm_anom_singleprecision_north.nc')
+    vod_anomaly = iris.load_cube(file_vod)
+    vod_anom = vod_anomaly.data.filled(np.nan)
+
+    file_sm = os.path.join(data_isv_dir, 'daily_detrended_sm_norm_anom_singleprecision_north.nc')
+    sm_anomaly = iris.load_cube(file_sm)
+    sm_anom = sm_anomaly.data.filled(np.nan)
+
+    data_comp_dir = output_dirs["data_isv_comp"]
+
     lat_south = 0
     lat_north = 60
     for land_cover in tqdm(land_covers):
         lc_events = events_for_land_cover(events, land_cover, lat_south, lat_north)
-        imerg_composite_south = np.load(f'../data/land_cover_isv_composites/imerg_composite_south_55NS_{land_cover}.npy')
-        vod_composite_south = np.load(f'../data/land_cover_isv_composites/vod_composite_south_55NS_{land_cover}.npy')
-        sm_composite_south = np.load(f'../data/land_cover_isv_composites/sm_composite_south_55NS_{land_cover}.npy')
-        n_south = np.load(f'../data/land_cover_isv_composites/n_south_55NS_{land_cover}.npy')
+
+        imerg_composite_south = np.load(os.path.join(data_comp_dir, f'imerg_composite_south_55NS_{land_cover}.npy'))
+        vod_composite_south = np.load(os.path.join(data_comp_dir, f'vod_composite_south_55NS_{land_cover}.npy'))
+        sm_composite_south = np.load(os.path.join(data_comp_dir, f'sm_composite_south_55NS_{land_cover}.npy'))
+        n_south = np.load(os.path.join(data_comp_dir, f'n_south_55NS_{land_cover}.npy'))
+
         days_around, imerg_composite, imerg_n = composite_events_all_valid(lc_events, imerg_anom, imerg_anom, vod_anom, sm_anom, 
                                                                            days_range=days_range, existing_composite=imerg_composite_south, existing_n=n_south)
         _, vod_composite, vod_n = composite_events_all_valid(lc_events, vod_anom, imerg_anom, vod_anom, sm_anom, 
                                                              days_range=days_range, existing_composite=vod_composite_south, existing_n=n_south)
         _, sm_composite, sm_n = composite_events_all_valid(lc_events, sm_anom, imerg_anom, vod_anom, sm_anom, 
                                                            days_range=days_range, existing_composite=sm_composite_south, existing_n=n_south)
+
         if np.logical_and(np.allclose(imerg_n, vod_n), np.allclose(imerg_n, sm_n)):
             n = imerg_n
         else:
             raise ValueError('ns are not the same')
-        np.save(f'../data/land_cover_isv_composites/imerg_composite_global_55NS_{land_cover}.npy', imerg_composite)
-        np.save(f'../data/land_cover_isv_composites/vod_composite_global_55NS_{land_cover}.npy', vod_composite)
-        np.save(f'../data/land_cover_isv_composites/sm_composite_global_55NS_{land_cover}.npy', sm_composite)
-        np.save(f'../data/land_cover_isv_composites/n_global_55NS_{land_cover}.npy', n)
+
+        file_out = os.path.join(data_comp_dir, f'imerg_composite_global_55NS_{land_cover}.npy')
+        np.save(file_out, imerg_composite)
+
+        file_out = os.path.join(data_comp_dir, f'vod_composite_global_55NS_{land_cover}.npy')
+        np.save(file_out, vod_composite)
+
+        file_out = os.path.join(data_comp_dir, f'sm_composite_global_55NS_{land_cover}.npy')
+        np.save(file_out, sm_composite)
+
+        file_out = os.path.join(data_comp_dir, f'n_global_55NS_{land_cover}.npy')
+        np.save(file_out, n)
 
 
-def ndvi_composites(land_covers, days_range=60):
+def ndvi_composites(output_dirs, land_covers, days_range=60):
+
+    data_isv_dir = output_dirs["data_isv"]
+    data_comp_dir = output_dirs["data_isv_comp"]
+
+    ###########################################################################
     # S Hem first
+    ###########################################################################
     lat_south = -60
     lat_north = 0
-    events = pickle.load(open(f'../data/imerg_isv_events_lowpass_1std_south.p','rb'))
-    ndvi_anomaly = iris.load(f'/scratch/bethar/monthly_detrended_ndvi_merged_norm_anom_singleprecision_south.nc')[0]
-    ndvi_anom = ma.filled(ndvi_anomaly.data.astype(np.float32), np.nan).astype(np.float32)
+
+    file_events = os.path.join(data_isv_dir, f'imerg_isv_events_lowpass_1std_south.pkl')
+    with open(file_events, 'rb') as f:
+        events = pickle.load(f)
+
+    file_ndvi = os.path.join(data_isv_dir, f'monthly_detrended_ndvi_merged_norm_anom_singleprecision_south.nc')
+    ndvi_anomaly = iris.load_cube(file_ndvi)
+    ndvi_anom = ndvi_anomaly.data.filled(np.nan)
+
     for land_cover in tqdm(land_covers):
         lc_events = events_for_land_cover(events, land_cover, lat_south, lat_north)
         days_around, ndvi_composite, ndvi_n = composite_events(lc_events, ndvi_anom, days_range=days_range)
-        np.save(f'../data/land_cover_isv_composites/ndvi/ndvi_composite_south_55NS_{land_cover}.npy', ndvi_composite)
-        np.save(f'../data/land_cover_isv_composites/ndvi/n_south_55NS_{land_cover}.npy', ndvi_n)
+
+        file_out = os.path.join(data_comp_dir, f'ndvi_composite_south_55NS_{land_cover}.npy')
+        np.save(file_out, ndvi_composite)
+
+        file_out = os.path.join(data_comp_dir, f'n_south_55NS_{land_cover}.npy')
+        np.save(file_out, ndvi_n)
+
+    ###########################################################################
     # add N Hem
+    ###########################################################################
     lat_south = 0
     lat_north = 60
-    events = pickle.load(open('../data/imerg_isv_events_lowpass_1std_north.p','rb'))
-    ndvi_anomaly = iris.load(f'/scratch/bethar/monthly_detrended_ndvi_merged_norm_anom_singleprecision_north.nc')[0]
-    ndvi_anom = ma.filled(ndvi_anomaly.data.astype(np.float32), np.nan).astype(np.float32)
+
+    file_events = os.path.join(data_isv_dir, f'imerg_isv_events_lowpass_1std_north.pkl')
+    with open(file_events, 'rb') as f:
+        events = pickle.load(f)
+
+    file_ndvi = os.path.join(data_isv_dir, f'monthly_detrended_ndvi_merged_norm_anom_singleprecision_north.nc')
+    ndvi_anomaly = iris.load_cube(file_ndvi)
+    ndvi_anom = ndvi_anomaly.data.filled(np.nan)
+
     for land_cover in tqdm(land_covers):
         lc_events = events_for_land_cover(events, land_cover, lat_south, lat_north)
-        ndvi_composite_south = np.load(f'../data/land_cover_isv_composites/ndvi/ndvi_composite_south_55NS_{land_cover}.npy')
-        n_south = np.load(f'../data/land_cover_isv_composites/ndvi/n_south_55NS_{land_cover}.npy')
+
+        ndvi_composite_south = np.load(os.path.join(data_comp_dir, f'ndvi_composite_south_55NS_{land_cover}.npy'))
+        n_south = np.load(os.path.join(data_comp_dir, f'n_south_55NS_{land_cover}.npy'))
+
         days_around, ndvi_composite, ndvi_n = composite_events(lc_events, ndvi_anom,
                                                                days_range=days_range, existing_composite=ndvi_composite_south,
                                                                existing_n=n_south)
-        np.save(f'../data/land_cover_isv_composites/ndvi/ndvi_composite_global_55NS_{land_cover}.npy', ndvi_composite)
-        np.save(f'../data/land_cover_isv_composites/ndvi/n_global_55NS_{land_cover}.npy', ndvi_n)
+
+        file_out = os.path.join(data_comp_dir, f'ndvi_composite_global_55NS_{land_cover}.npy')
+        np.save(file_out, ndvi_composite)
+
+        file_out = os.path.join(data_comp_dir, f'n_global_55NS_{land_cover}.npy')
+        np.save(file_out, ndvi_n)
 
 
 def make_patch_spines_invisible(ax):
@@ -213,7 +301,11 @@ def make_patch_spines_invisible(ax):
         sp.set_visible(False)
 
 
-def plot_composites(land_covers, days_range=60, ndvi=False):
+def plot_composites(output_dirs, land_covers, days_range=60, ndvi=False):
+
+    data_comp_dir = output_dirs["data_isv_comp"]
+    figures_dir = output_dirs["figures"]
+
     fig, axs = plt.subplots(3, 2, sharex=True, sharey=True, figsize=(9, 8))
     axlist = axs.flatten()
     blue =  '#56B4E9'
@@ -224,15 +316,17 @@ def plot_composites(land_covers, days_range=60, ndvi=False):
     days_around = np.arange(-days_range, days_range+1)
     for i, land_cover in enumerate(land_covers):
         ax = axlist[i]
-        imerg_composite = np.load(f'../data/land_cover_isv_composites/imerg_composite_global_55NS_{land_cover}.npy')
-        vod_composite = np.load(f'../data/land_cover_isv_composites/vod_composite_global_55NS_{land_cover}.npy')
-        sm_composite = np.load(f'../data/land_cover_isv_composites/sm_composite_global_55NS_{land_cover}.npy')
-        n = np.load(f'../data/land_cover_isv_composites/n_global_55NS_{land_cover}.npy')
+
+        imerg_composite = np.load(os.path.join(data_comp_dir, f'imerg_composite_global_55NS_{land_cover}.npy'))
+        vod_composite = np.load(os.path.join(data_comp_dir, f'vod_composite_global_55NS_{land_cover}.npy'))
+        sm_composite = np.load(os.path.join(data_comp_dir, f'sm_composite_global_55NS_{land_cover}.npy'))
+        n = np.load(os.path.join(data_comp_dir, f'n_global_55NS_{land_cover}.npy'))
+
         ax.plot(days_around, imerg_composite, color=blue, label='precipitation')
         ax.plot(days_around, sm_composite, color=brown, label='SSM')
         ax.plot(days_around, vod_composite, color=green, label='VOD')
         if ndvi:
-            ndvi_composite = np.load(f'../data/land_cover_isv_composites/ndvi/ndvi_composite_global_55NS_{land_cover}.npy')
+            ndvi_composite = np.load(os.path.join(data_comp_dir, f'ndvi_composite_global_55NS_{land_cover}.npy'))
             ax.plot(days_around, ndvi_composite, '--', color=light_green, label='NDVI')
         ax.set_ylim([-0.5, 1.75])
         ax.set_xticks(np.arange(-60, 61, 5), minor=True)
@@ -250,18 +344,37 @@ def plot_composites(land_covers, days_range=60, ndvi=False):
         ax.set_title(f'$\\bf{{({alphabet[i]})}}$ {land_cover_full_name(land_cover)}', fontsize=12)
         ax.text(58, 1.68, f'mean(n) = {int(np.round(np.mean(n)))}\nmin(n) = {int(np.min(n))}', ha='right', va='top')
     plt.tight_layout()
-    save_filename = '../figures/isv_composites/no_neighbours/vod_around_precip_isv_maxima_lowpass_1std_norm_withsm_subplots_global_55NS'
+
+    save_filename = os.path.join(figures_dir, 'vod_around_precip_isv_maxima_lowpass_1std_norm_withsm_subplots_global_55NS')
     if ndvi:
         save_filename += '_ndvi_dashed'
     plt.savefig(f'{save_filename}.png', dpi=400)
     plt.savefig(f'{save_filename}.pdf', dpi=400)
     plt.savefig(f'{save_filename}.eps', dpi=400)
-    plt.show()
+
+
+def main():
+
+    output_base_dir = "/path/to/output/dir"
+
+    output_dirs = {
+        "base": output_base_dir,
+        "spectra": os.path.join(output_base_dir, "csagan"),
+        "spectra_filtered": os.path.join(output_base_dir, "csagan_sig"),
+        "number_obs": os.path.join(output_base_dir, "number_obs_data"),
+        "pixel_time_series": os.path.join(output_base_dir, "data_pixel_time_series"),
+        "data_isv": os.path.join(output_base_dir, "data_isv"),
+        "data_isv_comp": os.path.join(output_base_dir, "data_isv_comp"),
+        "figures": os.path.join(output_base_dir, "figures"),
+    }
+
+    land_covers = ['baresparse', 'shrub', 'herb', 'crop', 'openforest', 'closedforest']
+
+    hemi_composites(output_dirs, 'south', land_covers)
+    full_composites(output_dirs, land_covers)
+    ndvi_composites(output_dirs, land_covers, days_range=60)
+    plot_composites(output_dirs, land_covers, days_range=60, ndvi=True)
 
 
 if __name__ == '__main__':
-    land_covers = ['baresparse', 'shrub', 'herb', 'crop', 'openforest', 'closedforest']
-    hemi_composites('south', land_covers)
-    full_composites(land_covers)
-    ndvi_composites(land_covers, days_range=60)
-    plot_composites(land_covers, days_range=60, ndvi=True)
+    main()
